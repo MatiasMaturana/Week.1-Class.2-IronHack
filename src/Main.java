1bd5e176fef0a12785e354cf9981f57ae10a2445
MatiasMaturana <dev.not-a@cougar.cat>
2022-07-28 19:54:39 +0200

    Initial commitment.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world! 2.0; now with vocative commas!");
+        System.out.println("Hello, world! 2.0; now multi-lingual and with vocative commas!");
     }
 }
