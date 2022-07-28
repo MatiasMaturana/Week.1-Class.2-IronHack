9d4a5f3dfcb780b42de5bf10905c71d749e14204
MatiasMaturana <dev.not-a@cougar.cat>
2022-07-28 20:07:59 +0200

    Initial commitment.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world! 2.0; now multi-lingual and with vocative commas!");
-        System.out.println("Hello world! 3.0; now with high version numbers!1");
+        System.out.println("Hello world! 3.0; now with high version numbers!!!");
         System.out.println("*Hello, world! 3.0; now with high version numbers!one");
     }
 }
