af2d1d8638df8d030ae7f26ced9dba9e141ba669
MatiasMaturana <dev.not-a@cougar.cat>
2022-07-28 20:22:44 +0200

    Initial comment, part deux: electric boogaloo.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world! 2.0; now multilingual and with vocative commas!");
         System.out.println("Hello world! 3.0; now with high version numbers!!!");
-        System.out.println("*Hello, world! 3.0; now with high version numbers!one");
+        System.out.println("*Hello, world! 3.0; now with high version numbers!");
     }
 }
